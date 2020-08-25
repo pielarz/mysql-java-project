5f3c201b9d19bad8c1443c8c0a80e48994872241
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-25 15:21:33 +0200

    DB connection

--- src/main/java/io/github/pielarz/dao/DBUtil.java
+++ src/main/java/io/github/pielarz/dao/DBUtil.java
@@ -1,4 +1,31 @@
 package io.github.pielarz.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Connection to the database.
+ *
+ * DB_URL holds and address to the database
+ *
+ * DB_NAME holds database name
+ *
+ * DB_PARAMS holds additional character encoding and ssl
+ *
+ * DB_USER holds username/login to database
+ *
+ * DB_PASSWORD holds password to database
+ */
+
 public class DBUtil {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    private static final String DB_NAME = "school";
+    private static final String DB_PARAMS = "?useSSL=false&characterEncoding=utf8";
+    private static final String DB_USER = "admin";
+    private static final String DB_PASSWORD = "12345678";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL + DB_NAME + DB_PARAMS, DB_USER, DB_PASSWORD);
+    }
 }
