24e688e5453ef5918efd2aa19ed5510830291abb
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-25 17:53:17 +0200

    User model

--- src/main/java/io/github/pielarz/model/User.java
+++ src/main/java/io/github/pielarz/model/User.java
@@ -1,4 +1,64 @@
 package io.github.pielarz.model;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class User {
+    private int id;
+    private String userName;
+    private String email;
+    private String password;
+    private Group group;
+
+    public User() {}
+
+    public User(String userName, String email, String password, Group group) {
+        this.userName = userName;
+        this.email = email;
+        this.hashPassword(password);
+        this.group = group;
+    }
+
+    public void hashPassword(String password) {
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.hashPassword(password);
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
 }
