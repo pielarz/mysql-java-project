fd0cc065df1f983bd9ff59fc08ed6bd6d1cd9b68
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-25 17:53:59 +0200

    Solution model

--- src/main/java/io/github/pielarz/model/Solution.java
+++ src/main/java/io/github/pielarz/model/Solution.java
@@ -1,4 +1,68 @@
 package io.github.pielarz.model;
 
+import java.time.LocalDateTime;
+
 public class Solution {
+    private int id;
+    private LocalDateTime created;
+    private LocalDateTime updated;
+    private String description;
+    private Exercise exercise;
+    private User user;
+
+    public Solution() {}
+
+    public Solution(User user, Exercise exercise) {
+        this.user = user;
+        this.exercise = exercise;
+        this.created = LocalDateTime.now();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
+    }
+
+    public void setCreated(LocalDateTime created) {
+        this.created = created;
+    }
+
+    public LocalDateTime getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(LocalDateTime updated) {
+        this.updated = updated;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Exercise getExercise() {
+        return exercise;
+    }
+
+    public void setExercise(Exercise exercise) {
+        this.exercise = exercise;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
