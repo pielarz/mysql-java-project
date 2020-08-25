a2d36ba56a6336cfaf85e8dce9345d26cb7ed4e4
Michał Pielarz <pielarz.michal@gmail.com>
2020-08-25 18:00:16 +0200

    Exercise DAO

--- src/main/java/io/github/pielarz/dao/ExerciseDao.java
+++ src/main/java/io/github/pielarz/dao/ExerciseDao.java
@@ -1,4 +1,96 @@
 package io.github.pielarz.dao;
 
+import io.github.pielarz.model.Exercise;
+
+import java.sql.*;
+import java.util.Arrays;
+
 public class ExerciseDao {
+    private static final String CREATE_EXERCISE_QUERY = "INSERT INTO exercises(title, description) VALUES (?,?)";
+    private static final String READ_EXERCISE_QUERY = "SELECT * FROM exercises where id = ?";
+    private static final String UPDATE_EXERCISE_QUERY = "UPDATE exercises SET title = ?, description = ? where id = ?";
+    private static final String DELETE_EXERCISE_QUERY = "DELETE FROM exercises WHERE id = ?";
+    private static final String FIND_ALL_EXERCISES_QUERY = "SELECT * FROM exercises";
+
+    public static Exercise create(Exercise exercise) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(CREATE_EXERCISE_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, exercise.getTitle());
+            statement.setString(2, exercise.getDescription());
+            statement.executeUpdate();
+            ResultSet resultSet = statement.getGeneratedKeys();
+            if (resultSet.next()) {
+                exercise.setId(resultSet.getInt(1));
+            }
+            return exercise;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Exercise read(int exerciseId) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(READ_EXERCISE_QUERY);
+            statement.setInt(1, exerciseId);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                Exercise exercise = new Exercise();
+                exercise.setId(resultSet.getInt("id"));
+                exercise.setTitle(resultSet.getString("title"));
+                exercise.setDescription(resultSet.getString("description"));
+                return exercise;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void update(Exercise exercise) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(UPDATE_EXERCISE_QUERY);
+            statement.setString(1, exercise.getTitle());
+            statement.setString(2, exercise.getDescription());
+            statement.setInt(3, exercise.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void delete(int exerciseId) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement statement = conn.prepareStatement(DELETE_EXERCISE_QUERY);
+            statement.setInt(1, exerciseId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Exercise[] addToArray(Exercise e, Exercise[] exercises) {
+        Exercise[] tmpExercises = Arrays.copyOf(exercises, exercises.length + 1);
+        tmpExercises[exercises.length] = e;
+        return tmpExercises;
+    }
+
+    public static Exercise[] findAll() {
+        try (Connection conn = DBUtil.getConnection()) {
+            Exercise[] exercises = new Exercise[0];
+            PreparedStatement statement = conn.prepareStatement(FIND_ALL_EXERCISES_QUERY);
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                Exercise exercise = new Exercise();
+                exercise.setId(resultSet.getInt("id"));
+                exercise.setTitle(resultSet.getString("title"));
+                exercise.setDescription(resultSet.getString("description"));
+                exercises = addToArray(exercise, exercises);
+            }
+            return exercises;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
